fdcb5fc8d550e6a77fb20c9933a99edb993e077d
NoraElshahat <nelshohat@gmail.com>
2021-07-11 13:41:49 +0200

    save work

--- to-do-list/frontend/my-app/src/component/header/Header.js
+++ to-do-list/frontend/my-app/src/component/header/Header.js
@@ -1,29 +1,38 @@
+import axios from 'axios';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 const Header = () => {
-  // function logout(e) {
-  //   e.preventDefault();
-  // }
+  let history = useHistory();
+  function signUp(e) {
+    e.preventDefault();
+    history.push('/sign-up');
+  }
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a class="navbar-brand">To Do List</a>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <Link to="/tasks" class="nav-item nav-link">
-              Tasks
-            </Link>
+    <>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a class="navbar-brand">To Do List</a>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link to="/tasks" class="nav-item nav-link">
+                Tasks
+              </Link>
+            </div>
           </div>
+          <form class="form-inline my-2 my-lg-0" onSubmit={signUp}>
+            {localStorage.getItem('freelance') == 'undefined'
+              ? 'Client ->  '
+              : 'Freelancer ->  '}
+            {localStorage.getItem('name') ? (
+              localStorage.getItem('name')
+            ) : (
+              <button className="btn btn-success">sign up</button>
+            )}
+          </form>
         </div>
-        <form class="form-inline my-2 my-lg-0">
-          {localStorage.getItem('freelance') == 'undefined'
-            ? 'Client ->  '
-            : 'Freelancer ->  '}
-          {localStorage.getItem('name')}
-          {/* <button className="btn btn-danger"> Log out</button> */}
-        </form>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
